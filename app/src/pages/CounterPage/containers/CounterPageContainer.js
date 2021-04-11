9bb4ee1a9a271109038406e3fce0dcabbe4091f5
AngelinaYatsevitch <3722600@gmail.com>
2021-04-11 16:01:21 +0300

    home work Counter

--- app/src/pages/CounterPage/containers/CounterPageContainer.js
+++ app/src/pages/CounterPage/containers/CounterPageContainer.js
@@ -7,35 +7,69 @@ class CounterPageContainer extends Component {
 
     this.state = {
       countValue: 0,
+      parityType: "even",
+    };
   }
-}
-  handleDecrement = ()=> {
-    this.setState({countValue: this.state.countValue-1});
+
+  componentDidUpdate() {
+     (this.state.parityType === "Введено нечетное число") ? alert("нечетное") : alert("четное"); // пока так, незнаю как добавить цвет
+    }
+
+  parityType = (value) => {
+    if (value % 2) {
+      return "Введено нечетное число";
+    } else {
+      return "Введено четное число";
+    }
   };
 
-  handleIncrement = ()=> {
-    this.setState({countValue: this.state.countValue+1});
+  handleIncrement = () => {
+    this.setState((state) => {
+      const countValue = state.countValue + 1;
+      const parityTypeEven = this.parityType(countValue);
+      return {
+        countValue,
+        parityType: parityTypeEven,
+      };
+    });
+  };
+
+  handleDecrement = () => {
+    this.setState((state) => {
+      if (this.state.countValue <= 0) {
+        this.setState({ countValue: (this.state.countValue = 0) });
+      } else {
+        const countValue = state.countValue - 1;
+        const parityTypeEven = this.parityType(countValue);
+        return {
+          countValue,
+          parityType: parityTypeEven,
+        };
+      }
+    });
   };
 
   handleReset = () => {
-    this.setState({countValue: this.state.countValue = 0});
-  }
-
-
-
+    this.setState((state) => {
+      const countValue = (state.countValue = 0);
+      return {
+        countValue,
+        parityType: "Введено четное число",
+      };
+    });
+  };
 
   render() {
     return (
-      <Counter 
-      countValue={this.state.countValue}
-      handleDecrement={this.handleDecrement}
-      handleIncrement={this.handleIncrement}
-      handleReset={this.handleReset}
-
+      <Counter
+        countValue={this.state.countValue}
+        parityType={this.state.parityType}
+        handleDecrement={this.handleDecrement}
+        handleIncrement={this.handleIncrement}
+        handleReset={this.handleReset}
       />
-    ); 
+    );
   }
 }
-
 
 export default CounterPageContainer;
